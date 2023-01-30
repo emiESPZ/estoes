c8e574a87189fc653ea0bd1880cfdcf85e632ae2
Emiliano Pérez <emi.esp86@gmail.com>
2023-01-29 22:21:41 -0300

    solución texto en  dos lineas en My Projects

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProjectList from './pages/ProjectList';
 import AddProject from './pages/AddProject';
 import EditProject from './pages/EditProject';
-import { ProjectsProvider } from './contexts/ProjectContext';
+
 import NavBar from './components/NavBar';
 import './App.css';
 
 const App = () => {
   return (
-    <ProjectsProvider>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route exact path='/' element={<ProjectList />} />
-          <Route path='/create' element={<AddProject />} />
-          <Route path='/edit/:id' element={<EditProject />} />
-        </Routes>
-      </BrowserRouter>
-    </ProjectsProvider>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<ProjectList />} />
+        <Route path='/create' element={<AddProject />} />
+        <Route path='/edit/:id' element={<EditProject />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
